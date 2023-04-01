87e6760f3c594e3895e2ab0c2226580e0a782929
foreverxiaoyu <forever_love233@163.com>
2023-03-31 23:15:24 -0400

    Create teest.py

---
+++ trust_region/smooth_newton_method/teest.py
@@ -0,0 +1,28 @@
+import math
+
+import numpy as np
+
+
+def phi(mu, a, b):
+
+    return p
+
+
+def dah(n, mu, lam, d, gk, Bk, deltak):
+
+    return H
+
+
+def JacobiH(n, mu, lam, d, Bk, deltak):
+
+    return J
+
+
+def psi(n, mu, lam, d, gk, Bk, deltak, gamma):
+
+    return si
+
+
+def smooth_newton_method(n,f, gk, Bk, deltak):
+
+    return d
